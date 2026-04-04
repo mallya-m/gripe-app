3d5ba2195d9d0f65e980da996e7c302e20c1eff7
Mallya <mallyamoni035@gmail.com>
2026-04-04 19:39:01 +0530

    chore: clean project structure and mongodb setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const express = require('express');
+
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Gripe API is running' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
